f82cc25a05c8e6d20f14d9af4d212628e6e276bf
Rohit Sharma <rohitsharma@Rohits-MacBook-Air.local>
2024-09-17 13:46:36 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+
+const express = require('express');
+const mongoose = require('mongoose');
+const workoutRoutes = require('./routes/workouts')
+const userRoutes = require('./routes/user');
+//express app
+const app = express();
+
+//middleware
+app.use(express.json());
+
+
+app.use((req ,res , next)=>{
+    console.log(`Request made at ${req.method} ${req.url}`);
+    next();
+})
+
+//routes
+app.use('/api/workouts' , workoutRoutes);
+app.use('/api/user' , userRoutes);
+
+//connect to db
+mongoose.connect(process.env.MONG_URI)
+   .then(()=> {
+    //listen for requests
+    app.listen(process.env.PORT, ()=>{
+    console.log(`Connected to db & Server is running on port` , process.env.PORT);
+    })
+   })
+   .catch(err=> console.error(err));
